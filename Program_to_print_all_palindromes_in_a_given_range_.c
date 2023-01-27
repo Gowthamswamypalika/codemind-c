b9991486c4393d14c4034a33ee8f1f0a1fc426bb
Gowthamswamypalika <120080083+Gowthamswamypalika@users.noreply.github.com>
2023-01-27 11:31:40 +0530

    #include<stdio.h> int all(int); int main() {     int a,b,i,n;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         if(all(i))         {             printf("%d ",i);         }     } }  int all(int a)     {         int r,rev=0,t;         t=a;         while(a!=0)         {             r=a%10;             rev=rev*10+r;             a=a/10;         }         if(rev==t)         {             return rev;         }         else         {             return 0;         }     }

---
+++ Program_to_print_all_palindromes_in_a_given_range_.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int all(int);
+int main()
+{
+    int a,b,i,n;
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        if(all(i))
+        {
+            printf("%d ",i);
+        }
+    }
+}
+ int all(int a)
+    {
+        int r,rev=0,t;
+        t=a;
+        while(a!=0)
+        {
+            r=a%10;
+            rev=rev*10+r;
+            a=a/10;
+        }
+        if(rev==t)
+        {
+            return rev;
+        }
+        else
+        {
+            return 0;
+        }
+    }
